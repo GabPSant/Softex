13d06d1aeb6a1aaf137a7422248fd6b1e291ce1f
Gabriel P. Santos <gabrielp_santos@hotmail.com>
2023-08-02 23:29:26 -0300

    Início da 'atividade5Parte2.js'

--- Atividades-Presencial/JavaScript/atividade5Parte1.js
+++ Atividades-Presencial/JavaScript/atividade5Parte1.js
@@ -1,6 +1,6 @@
 // Questão 1 - funciona usando node
 //const inteiro = Number(prompt("Digite um número inteiro"));
-function questao1(inteiro){
+function questao1(inteiro){//Diz se o valor digitado é positivo, negativo ou neutro
     if(inteiro>0){
         alert(`O valor digitado (${inteiro}) é positivo`)
     }
@@ -12,7 +12,7 @@ function questao1(inteiro){
 //questao1(inteiro);
 
 // Questão 2 - funciona usando node
-function maiorDeIdade(idade){
+function maiorDeIdade(idade){// Diz se alguem é menor ou maior de idade
     if(idade<0){
         console.log("Você não tem idade negativa");
     }
@@ -24,7 +24,7 @@ function maiorDeIdade(idade){
 // maiorDeIdade(-7);
 
 // Questão 3 - é necessário usar no console web
-function questao3(){
+function questao3(){// Avalia qual entre dois números é maior
     const inteiro1 = Number(prompt("Digite o primeiro inteiro"));
     const inteiro2 = Number(prompt("Digite o segundo inteiro"));
     
@@ -39,7 +39,7 @@ function questao3(){
 // questao3();
 
 // Questão 4 - é necessário usar no console web
-function questao4(){
+function questao4(){// Retorna se um número é par ou impar
     const numero = parseInt(prompt("Digite um numero"));
     if(Number.isNaN(numero)){//testa se o valor não é um numero
         alert("O valor é invalido")
@@ -49,8 +49,9 @@ function questao4(){
 // questao4();
 
 // Questão 5 - é necessário fazer no console web
-let getMedia = (x,y,z) => (x+y+z)/3
+let getMedia = (x,y,z) => (x+y+z)/3 // calculo para a media de 3 valores
 function questao5(){
+    // Diz se um aluno está aprovado, em relação a media de 3 notas
     const nota1 = parseFloat(prompt("Digite a primeira nota"));
     const nota2 = parseFloat(prompt("Digite a segunda nota"));
     const nota3 = parseFloat(prompt("Digite a terceira nota"));
@@ -69,27 +70,31 @@ function questao5(){
 // questao5();
 
 // Questão 6 - funciona usando node
-function questao6(p1, p2){
-    if(String(p1).length == String(p2).length){
+function questao6(pessoa1, pessoa2){
+    // Avalia qual nome de duas pessoas é maior, tem mais letras
+    if(String(pessoa1).length == String(pessoa2).length){
         console.log("Ambas as strings tem mesmo tamanho");
     }
     else{
-        console.log((String(p1).length>String(p2).length)?
-        `${p1} é maior que ${p2}`:`${p2} é maior que ${p1}`);
+        console.log((String(pessoa1).length>String(pessoa2).length)?
+        `${pessoa1} é maior que ${pessoa2}`:`${pessoa2} é maior que ${pessoa1}`);
     }
 }
-// questao6("Maior","Melhor");
+// questao6("Miguel","Rafael");
 
 // Questão 7 - (testar) é necessário fazer no console web
 function questao7(){
+    // Avalia se um caracter é vogal
+    let caracter;
     while(true){
-        const caracter = prompt("Digite um caracter (deve ser uma letra)");
+        caracter = prompt("Digite um caracter (deve ser uma letra)");
         try{
             if(caracter.length>=2){throw Error("Deve ser somente um caractere");}
             if(!Number.isNaN(Number(caracter))){throw Error("É um número, não vale")}
+            break;
         }
         catch(error){alert("Esse valor não é valido");}
-        break;
     }
     alert((caracter.match(/[(a|e|i|o|u)]/g)?`${caracter} é vogal`:`${caracter} não é vogal`));
 }
+// questao7();
