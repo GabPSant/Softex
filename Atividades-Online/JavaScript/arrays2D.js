45d03597d0a1769f848c1154691777c3134e024a
Gabriel P. Santos <gabrielp_santos@hotmail.com>
2023-08-06 23:44:38 -0300

    leves alterações no 'arrays2D.js'

---
+++ Atividades-Online/JavaScript/arrays2D.js
@@ -0,0 +1,10 @@
+// Array contendo animais
+const animais = [
+    ['Apolo', 'Cachorro', 7],
+    ['Jack', 'Canguru', 13],
+    ['Clarice','Gato', 10],
+    ['Hedwig', 'Coruja', 14]
+];
+console.log("Lista completa:\n",animais,"\n\n1º animal:\n",animais[0],"\n2º animal:\n",animais[1],
+"\n3º animal:\n",animais[2],"\n4º animal:\n",animais[3]);
+//Cada array dentro de animais contem o nome, especie e idade de um animal específico.
